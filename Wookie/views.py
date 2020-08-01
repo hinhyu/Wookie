522cde253c528760b7bb2abd85b18982a45b87f5
HongInhye <hinhyu@naver.com>
2020-08-02 02:44:20 +0900

    text list

--- Wookie/views.py
+++ Wookie/views.py
@@ -22,7 +22,8 @@ def art(request):
 
 def other(request):
     objs = Post.objects
-    return render(request, 'other.html', {'obj' : objs})
+    user = get_object_or_404(Profile, pk = user_id)
+    return render(request, 'other.html', {'obj' : objs, 'user':user})
 
 def add_comment(request, pk, user_id):
     post = get_object_or_404(Post, pk = pk)
